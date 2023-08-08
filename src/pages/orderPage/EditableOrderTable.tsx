3b3d63511e0246e438f318e572e76fda35b62cb1
marcusjacobsen <S215799@dtu.dk>
2023-08-08 22:31:17 +0200

    Beginning on input in order/budget table

--- src/pages/orderPage/EditableOrderTable.tsx
+++ src/pages/orderPage/EditableOrderTable.tsx
@@ -2,7 +2,15 @@ import React, {useEffect, useState} from "react";
 import {collection, doc, DocumentReference, getDocs, query, where} from "firebase/firestore";
 import {auth, db} from "../../firebase/firebase";
 import {onAuthStateChanged} from "firebase/auth";
-import {ColumnDef, createColumnHelper, flexRender, getCoreRowModel, useReactTable} from "@tanstack/react-table";
+import {
+    ColumnDef,
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+    RowData,
+    getFilteredRowModel, getPaginationRowModel
+} from "@tanstack/react-table";
 
 type Order = {
     amount:number;
@@ -36,6 +44,55 @@ type Season = {
     id:string;
 }
 
+declare module '@tanstack/react-table' {
+    interface TableMeta<TData extends RowData> {
+        updateData: (rowIndex: number, columnId: string, value: unknown) => void
+    }
+}
+
+// Give our default column cell renderer editing superpowers!
+const defaultColumn: Partial<ColumnDef<{ customer:Customer,wholeSeason:{season:Season,budget:number,order:number}[]}>> = {
+    cell: ({ getValue, row: { index }, column: { id }, table }) => {
+        const initialValue = getValue()
+        // We need to keep and update the state of the cell normally
+        const [value, setValue] = React.useState(initialValue)
+
+        // When the input is blurred, we'll call our table meta's updateData function
+        const onBlur = () => {
+            table.options.meta?.updateData(index, id, value)
+        }
+
+        // If the initialValue is changed external, sync it up with our state
+        React.useEffect(() => {
+            setValue(initialValue)
+        }, [initialValue])
+
+        return (
+            <input
+                value={value as string}
+                onChange={e => setValue(e.target.value)}
+                onBlur={onBlur}
+            />
+        )
+    },
+}
+
+function useSkipper() {
+    const shouldSkipRef = React.useRef(true)
+    const shouldSkip = shouldSkipRef.current
+
+    // Wrap a function with this to skip a pagination reset temporarily
+    const skip = React.useCallback(() => {
+        shouldSkipRef.current = false
+    }, [])
+
+    React.useEffect(() => {
+        shouldSkipRef.current = true
+    })
+
+    return [shouldSkip, skip] as const
+}
+
 const columnHelper = createColumnHelper<{ customer:Customer,wholeSeason:{season:Season,budget:number,order:number}[]}>();
 
 
@@ -48,6 +105,7 @@ function EditableOrderTable(){
     const [selectedSeasons, setSelectedSeasons] = useState<{firstIndex:number,lastIndex:number}>({firstIndex:0,lastIndex:0});
     const [data, setData] = useState<{ customer:Customer,wholeSeason:{season:Season,budget:number,order:number}[]}[]>([]);
     const [showDelta, setShowDelta] = useState<boolean>(false);
+    const [autoResetPageIndex, skipAutoResetPageIndex] = useSkipper()
     const [columns, setColumns] = useState([
         columnHelper.accessor("customer", {
             header: "Customer",
@@ -250,11 +308,26 @@ function EditableOrderTable(){
                     const extraColumns = [
                         columnHelper.accessor("wholeSeason", {
                             header: season.name + " Budget",
-                            cell: (info) => {
-                                const seasonInfo = info.getValue().find((s) => s.season.name === season.name)
+                            cell: ({ getValue, row: { index }, column: { id }, table }) => {
+                                let initialValue : string = "0";
+                                const seasonInfo = getValue().find((s) => s.season.name === season.name)
                                 if (seasonInfo)
-                                    return parseFloat(seasonInfo.budget.toString()).toLocaleString() + " " + selectedBrand?.currency;
-                                else return 0;
+                                    initialValue = parseFloat(seasonInfo.budget.toString()).toLocaleString();
+
+                                const [value, setValue] = React.useState(initialValue)
+                                return(
+                                    <div>
+                                <input
+                                    value={value}
+                                    onChange={e => {
+                                        const value = e.target.value
+                                        setValue(value)
+                                        table.setGlobalFilter([{ id, value }])
+                                    }
+                                    }
+                                />
+                                        {selectedBrand?.currency}
+                                    </div>)
                             },
                             footer: info => info.column.id,
                             id: season.name + "Budget",
@@ -317,8 +390,30 @@ function EditableOrderTable(){
     const table = useReactTable(
         {
             columns: columns,
+            defaultColumn: defaultColumn,
             data: data,
             getCoreRowModel: getCoreRowModel(),
+            getFilteredRowModel: getFilteredRowModel(),
+            getPaginationRowModel: getPaginationRowModel(),
+            autoResetPageIndex,
+            meta: {
+                updateData: (rowIndex, columnId, value) => {
+                    // Skip page index reset until after next rerender
+                    skipAutoResetPageIndex()
+                    setData(old =>
+                        old.map((row, index) => {
+                            if (index === rowIndex) {
+                                return {
+                                    ...old[rowIndex]!,
+                                    [columnId]: value,
+                                }
+                            }
+                            return row
+                        })
+                    )
+                },
+            },
+            debugTable: true,
         }
     );
 
@@ -330,7 +425,7 @@ function EditableOrderTable(){
     }
 
     return <div className="customers">
-        <h1>Customers By Brand</h1>
+        <h1>Set Orders</h1>
         <div className="selection-wrapper">
             <div className="selection">
                 <label htmlFor="brand"></label>
